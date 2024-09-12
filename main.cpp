ec3f3c9ab0e03d87de581041d1afd5551e758bb2
Ilia Khidri <s058048@ad.viko.lt>
2024-09-12 11:06:47 +0300

    Antras uzduotis

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    string vardas = "Ilia";
+    /*string vardas = "Ilia";
     string pavarde = "Khidri";
     int amzius = 19;
     string grupe = "PI24";
@@ -18,7 +18,20 @@ int main()
     cout << "Amzius: " << amzius << '\n';
     cout << "Grupe: " << grupe << '\n';
     cout << "Kursas: " << kursas << '\n';
-    cout << "Studiju programa: " << studiju_programa << '\n';
+    cout << "Studiju programa: " << studiju_programa << '\n';*/
+
+    string klubo_pavadinimas = "Real Madrid";
+    int ikurimo_metai = 1902;
+    string vadovas = "Karlas Ancelotis";
+    string arena_stadionas = "\"Santiago Bernabeu\" stadionas";
+    int arenos_vietu_skaicius = 85000;
+
+    cout << "Sporto klubo informacija: " << '\n';
+    cout << "Pavadinimas: " << klubo_pavadinimas << '\n';
+    cout << "Ikurimo metai: " << ikurimo_metai << '\n';
+    cout << "Vadovas: " << vadovas << '\n';
+    cout << "Arena / Stadionas: " << arena_stadionas << '\n';
+    cout << "Arenos / Stadiono vietu skaicius: " << arenos_vietu_skaicius << '\n';
 
     return 0;
 }
