14bf5e6d8fcda67573e7f5ec166665a80c8ece63
Ilia Khidri <s058048@ad.viko.lt>
2024-09-12 10:52:18 +0300

    Sukurtas main.cpp failas

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string vardas = "Ilia";
+    string pavarde = "Khidri";
+    int amzius = 19;
+    string grupe = "PI24";
+    int kursas = 1;
+    string studiju_programa = "Programu sistemos";
+
+    cout << "Studento informacija: " << '\n';
+    cout << "Vardas: " << vardas << '\n';
+    cout << "Pavarde: " << pavarde << '\n';
+    cout << "Amzius: " << amzius << '\n';
+    cout << "Grupe: " << grupe << '\n';
+    cout << "Kursas: " << kursas << '\n';
+    cout << "Studiju programa: " << studiju_programa << '\n';
+
+    return 0;
+}
